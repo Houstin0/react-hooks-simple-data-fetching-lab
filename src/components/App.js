ad80014d756411ace79666bb4879c65e9fb3a639
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-21 18:10:06 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React,{useState,useEffect} from "react";
+const App = () => {
+    
+    const[dogImage,SetDogImage]=useState(null)
+    const[isLoaded,SetisLoaded]=useState(false)
+    useEffect(() => {
+
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+
+            SetDogImage(data.message)
+            SetisLoaded(true)
+        })
+    },[])
+
+
+        if (!isLoaded) {
+            return  <p>Loading...</p>
+        }else{
+            return  <img src={dogImage} alt='A Random Dog'></img>
+        }
+         
+        
+
+
+
+}
+export default App;
